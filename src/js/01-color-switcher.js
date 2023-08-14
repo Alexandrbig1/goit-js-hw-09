8c7411081febcb1cb63c617d018cddad6960fe49
Alex Smagin <alexsmagin1@gmail.com>
2023-08-14 11:24:52 -0500

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  changeColor();
+  // const timeId = setInterval(onStartBtnClick, 1000);
+}
+
+function onStopBtnClick() {
+  clearInterval(timeId);
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
