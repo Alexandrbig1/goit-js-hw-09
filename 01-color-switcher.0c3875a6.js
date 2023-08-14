65a1202da1392153a24e3f79efa7224ec02f6a9b
Alexandrbig1 <107826794+Alexandrbig1@users.noreply.github.com>
2023-08-14 16:25:19 +0000

    Deploying to gh-pages from @ Alexandrbig1/goit-js-hw-09@8c7411081febcb1cb63c617d018cddad6960fe49 🚀

---
+++ 01-color-switcher.0c3875a6.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};t.startBtn.addEventListener("click",(function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))})),t.stopBtn.addEventListener("click",(function(){clearInterval(timeId)}))}();
+//# sourceMappingURL=01-color-switcher.0c3875a6.js.map
